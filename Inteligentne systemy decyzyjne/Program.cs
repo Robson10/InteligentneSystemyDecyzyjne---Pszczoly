c849f4e6cefeba998f5258cdd3b02d853d08476e
Mucha <mucharobert23@gmail.com>
2018-01-21 19:33:38 +0100

    Add project files.

---
+++ Inteligentne systemy decyzyjne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inteligentne_systemy_decyzyjne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
